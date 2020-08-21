c6cc2d87d50ac70a3a3472422613f6e5950e48d1
Neeraj Adhikari <neeraj@uri.edu>
2020-08-21 10:58:17 -0400

    Port OutputWriter and its implementers to C#
    Port the OutputWriter interface and its implementing classes (FileWriter
    and SheetWriter) to C#.

---
+++ csplugin/BOOTH/IOutputWriter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOTH
+{
+    interface IOutputWriter
+    {
+        void WriteLineArr(string[] line);
+
+        void WriteLine(params string[] line);
+
+        long GetRowNum();
+
+        void Done();
+    }
+}
---
+++ csplugin/BOOTH/SheetWriter.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOOTH
+{
+    class SheetWriter : IOutputWriter
+    {
+        private readonly Worksheet sheet;
+        private long rowNum;
+
+        public SheetWriter(Worksheet sheet)
+        {
+            this.sheet = sheet;
+            this.rowNum = 1;
+        }
+
+        public void Done()
+        {
+            FormatPretty();
+        }
+
+        public void FormatPretty()
+        {
+            int columns = this.sheet.UsedRange.Columns.Count;
+            this.sheet.Range["A1", Util.getLetterFromNumber(columns) + "1"].Font.Bold = true;
+            this.sheet.UsedRange.Columns.AutoFit();
+        }
+
+        public long GetRowNum()
+        {
+            return rowNum;
+        }
+
+        public void WriteLine(params string[] line)
+        {
+            this.WriteLineArr(line);
+        }
+
+        public void WriteLineArr(string[] line)
+        {
+            string rangeEnd = Util.getLetterFromNumber(line.Length) + rowNum;
+            this.sheet.Range["A" + rowNum, rangeEnd].Value = line;
+            rowNum++;
+        }
+    }
+}
